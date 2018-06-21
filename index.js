802ee7653b9aec65accb419cc5d00dd2a1c76737
Luciana M <lucianamonsynhatti@gmail.com>
2018-06-21 12:05:05 -0300

    Finalizar

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
+const BodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser)
+
 app.get("/pudim", (req,res) => {
     res.send({
         sabor: "leite",
@@ -8,5 +11,11 @@ app.get("/pudim", (req,res) => {
         frescura: "extrato de baunilha"
     });
 });
+app.get("/teste",(req,res) => {
+    res.send(("<h1> TESTE!!</h1>"));
+});
 
+app.post("/",(req,res) => {
+    res.send(("recebi um POST !!!"));
+});
 app.listen(3000);
